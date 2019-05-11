2df3f58a476837198ab929db679bf4e5be8a1354
mwalanay@gmail.com <mwalanay@gmail.com>
2019-05-11 22:11:23 +0700

    ketiga

---
+++ HSClockStyle/app/src/main/java/illegaller/aida/dev/art/clocks/ClocksWidget.java
@@ -0,0 +1,30 @@
+package illegaller.aida.dev.art.clocks;
+import android.appwidget.*;
+import android.content.*;
+import android.app.*;
+import android.widget.*;
+
+public class ClocksWidget extends AppWidgetProvider
+{
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        // TODO: Implement this method
+        for(int appwidgetId : appWidgetIds)
+        {
+            Intent intent = new Intent(context,  MainActivity.class);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews viss = new RemoteViews(context.getPackageName(), R.layout.clocks_widget);
+            viss.setOnClickPendingIntent(R.id.frame_container, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appwidgetId,  viss);
+
+        }
+    }
+
+
+}
+
